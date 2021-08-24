a61760b67ab2877293b9c2a7ae4012e1f2fec66e
hitendraDongare <hitendra.dongare@gmail.com>
2021-08-24 21:10:09 +0530

    Update HashtabelTest1.java

--- map/hashtableTest/HashtabelTest1.java
+++ map/hashtableTest/HashtabelTest1.java
@@ -1,1 +1,28 @@
+package hashtabelTest;
+
+import java.util.Hashtable;
+
+public class HashtabelTest1 {
+
+	public static void main(String[] args) {
+		
+		Hashtable<Integer, String>m = new Hashtable<Integer, String>();
+		
+		m.put(1, "Pankaj");
+		m.put(4, "Chetan");
+		m.put(3, "Amit");
+		m.put(2, "Pratik");
+		System.out.println(m);
+		
+		System.out.println(m.getOrDefault(6, "value not found"));
+		
+		m.putIfAbsent(5, "Narendra");
+		System.out.println(m);
+		
+		m.putIfAbsent(4, "Vijay");
+		System.out.println(m);
+
+	}
+
+}
 
